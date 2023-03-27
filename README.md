c809c77c9b30ab86ca84f8bb1e76f26323dda31d
Claudio Lau <claudio.lau12@gmail.com>
2023-03-27 11:44:33 -0400

    updated data env and makefile

--- README.md
+++ README.md
@@ -3,3 +3,15 @@
 # Getting Started
 
 `pnpm i`
+
+# Maintenance
+
+[prisma](https://www.prisma.io/docs/getting-started/quickstart)
+
+Prisma is an open source next-generation ORM. It consists of the following parts:
+
+Prisma Client: Auto-generated and type-safe query builder for Node.js & TypeScript
+
+Prisma Migrate: Migration system
+
+Prisma Studio: GUI to view and edit data in your database.
--- src/components/Content/HomeContent.tsx
+++ src/components/Content/HomeContent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import { ProductData } from "@/types";
 import { FC } from "react";
 
@@ -11,29 +12,49 @@ export const Content: FC<Partial<ProductData[]>> = (props) => {
       <nav className="flex flex-col items-center justify-between w-full relative max-w-7xl  mx-auto pt-8 pb-8 text-gray-900 ">
         <div className="flex flex-col w-full lg:flex-row p-4">
           <div className="grid h-32 card rounded-box mr-4">
-            <img src={mockContent[0]?.image} className="rounded" />
+            <img
+              src={String(mockContent[0]?.image)}
+              className="rounded"
+              alt="0"
+            />
             <span> {mockContent[0]?.name}</span>
           </div>
           <div className=" p-4" />
           <div className="grid card rounded-box">
-            <img src={mockContent[1]?.image} className="rounded" />
+            <img
+              src={String(mockContent[1]?.image)}
+              className="rounded"
+              alt="1"
+            />
             <span> {mockContent[1]?.name}</span>
           </div>
         </div>
         <div className="flex flex-col p-4">
           <div className="grid card rounded-box">
-            <img src={mockContent[2]?.image} className="rounded" />
+            <img
+              src={String(mockContent[2]?.image)}
+              className="rounded"
+              alt="2"
+            />
             <span> {mockContent[2]?.name}</span>
           </div>
         </div>
         <div className="flex flex-col w-full lg:flex-row p-4">
           <div className="grid card rounded-box">
-            <img src={mockContent[3]?.image} className="rounded" />
+            <img
+              src={String(mockContent[3]?.image)}
+              className="rounded"
+              alt="3"
+            />
             <span> {mockContent[3]?.name}</span>
           </div>
           <div className=" p-4" />
           <div className="grid card rounded-box">
-            <img src={mockContent[4]?.image} className="rounded" />
+            <img
+              src={String(mockContent[4]?.image)}
+              className="rounded"
+              alt="4"
+            />
             <span> {mockContent[4]?.name}</span>
           </div>
         </div>
