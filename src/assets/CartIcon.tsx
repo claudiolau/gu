6788c7a008b868cda8f9ee96380e86a785508f90
Claudio Lau <claudio.lau12@gmail.com>
2023-04-09 20:42:14 -0400

    updated svg icoin

--- src/assets/CartIcon.tsx
+++ src/assets/CartIcon.tsx
@@ -1,6 +1,4 @@
-import { SVGProps } from "react";
-
-export const CartIcon = (): SVGProps<SVGSVGElement> => {
+export const CartIcon = () => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
