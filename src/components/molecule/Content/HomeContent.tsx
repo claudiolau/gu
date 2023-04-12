98311d9e8a79b7aa19760c4857d1a61f22ceaa27
Claudio Lau <claudio.lau12@gmail.com>
2023-04-11 23:39:05 -0400

    refactoring

--- src/components/molecule/Content/HomeContent.tsx
+++ src/components/molecule/Content/HomeContent.tsx
@@ -10,7 +10,7 @@ export const HomeContent: FC<Product[]> = (props) => {
   return (
     <section className="flex-grow bg-[#FDFBF3]">
       <div className="flex flex-col items-center justify-between w-full relative max-w-7xl mx-auto pt-8 pb-8 text-gray-900">
-        <div className="grid grid-cols-3 gap-4 p-4">
+        {/* <div className="grid grid-cols-3 gap-4 p-4">
           <div className="bg-white rounded-lg shadow-md p-4">
             <img
               src="https://www.soph.net/img/goods/L/fcrb_230010_black.jpg"
@@ -47,7 +47,7 @@ export const HomeContent: FC<Product[]> = (props) => {
               alt="description of your image"
             />
           </div>
-        </div>
+        </div> */}
       </div>
     </section>
   );
