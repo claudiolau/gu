1694ac2dee18cacbb1f68c52fd9ef464871675b1
Claudio Lau <claudio.lau12@gmail.com>
2023-04-11 23:29:46 -0400

    made a working slider

--- src/components/atoms/Hero/Hero.tsx
+++ src/components/atoms/Hero/Hero.tsx
@@ -1,51 +1,59 @@
+import { useState } from "react";
+
 export const Hero = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
   const carouselItems = [
     {
       id: "slide1",
       imgUrl:
-        "https://daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg",
-      prevItemId: "slide4",
+        "https://www.soph.net/img/usr/visual/main_sophnet_gregory_23ss_pc1.jpg",
+      prevItemId: "slide3",
       nextItemId: "slide2",
     },
     {
       id: "slide2",
-      imgUrl:
-        "https://daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg",
+      imgUrl: "https://www.soph.net/img/usr/visual/main_soph_23ss_pc16.jpg",
       prevItemId: "slide1",
       nextItemId: "slide3",
     },
     {
       id: "slide3",
-      imgUrl:
-        "https://daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg",
+      imgUrl: "https://www.soph.net/img/usr/visual/main_soph_23ss_pc15.jpg",
       prevItemId: "slide2",
-      nextItemId: "slide4",
-    },
-    {
-      id: "slide4",
-      imgUrl: "/images/stock/photo-1665553365602-b2fb8e5d1707.jpg",
-      prevItemId: "slide3",
       nextItemId: "slide1",
     },
   ];
 
+  const handleClickPrev = () => {
+    const prevSlideIndex =
+      currentSlide === 0 ? carouselItems.length - 1 : currentSlide - 1;
+    setCurrentSlide(prevSlideIndex);
+  };
+
+  const handleClickNext = () => {
+    const nextSlideIndex = (currentSlide + 1) % carouselItems.length;
+    setCurrentSlide(nextSlideIndex);
+  };
+
   return (
     <>
       <div className="carousel w-full">
-        {carouselItems.map((item) => (
+        {carouselItems.map((item, index) => (
           <div
             key={item.id}
             id={item.id}
-            className="carousel-item relative w-full"
+            className={`carousel-item relative w-full ${
+              index === currentSlide ? "active" : ""
+            }`}
           >
-            <img src={item.imgUrl} className="w-full" />
+            <img src={carouselItems[currentSlide].imgUrl} className="w-full" />
             <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href={`#${item.prevItemId}`} className="btn btn-circle">
+              <button onClick={handleClickPrev} className="btn btn-circle">
                 ❮
-              </a>
-              <a href={`#${item.nextItemId}`} className="btn btn-circle">
+              </button>
+              <button onClick={handleClickNext} className="btn btn-circle">
                 ❯
-              </a>
+              </button>
             </div>
           </div>
         ))}
