4dff44017af02f7d80c781bee53e4bc1f2d6beba
Claudio Lau <claudio.lau12@gmail.com>
2023-04-13 13:06:19 -0400

    next content

--- src/components/molecule/Content/HomeContent.tsx
+++ src/components/molecule/Content/HomeContent.tsx
@@ -24,14 +24,14 @@ export const HomeContent: FC<Product[]> = (props) => {
         <div className="text-center text-3xl text-[#4B4A46] m-4 uppercase">
           SHOP BY CATEGORIES
         </div>
-        <div className="grid grid-cols-3 gap-4 p-4 h-3/6 sm:grid-cols-3 sm:h-3/6">
+        <div className="grid grid-cols-3 gap-4 h-3/6 sm:grid-cols-3 sm:h-3/6 space-x-4 pb-24 text-2xl">
           <div className="bg-white rounded-lg shadow-md">
             <img
               className="h-full w-full object-cover"
               src="https://images.unsplash.com/photo-1516762689617-e1cffcef479d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=822&q=80"
               alt="description of your image"
             />
-            <figcaption className="text-center ">Clothing</figcaption>
+            <figcaption className="text-center p-8">Clothing</figcaption>
           </div>
           <div className="bg-white rounded-lg shadow-md">
             <img
@@ -39,7 +39,7 @@ export const HomeContent: FC<Product[]> = (props) => {
               src="https://static.nike.com/a/images/f_auto/dpr_3.0,cs_srgb/h_500,c_limit/64265cce-0cfc-4309-aa45-33835124eeaa/the-best-nike-high-top-sneakers-you-can-buy-right-now-articles-ogc.jpg"
               alt="shoes"
             />
-            <figcaption className="text-center">Shoes</figcaption>
+            <figcaption className="text-center p-8">Shoes</figcaption>
           </div>
           <div className="bg-white rounded-lg shadow-md">
             <img
@@ -47,27 +47,26 @@ export const HomeContent: FC<Product[]> = (props) => {
               src="https://plus.unsplash.com/premium_photo-1673548917043-9facc637bf37?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"
               alt="description of your image"
             />
-            <figcaption className="text-center">Furniture</figcaption>
+            <figcaption className="text-center p-8">Furniture</figcaption>
           </div>
         </div>
-        <div className="grid grid-cols-2 gap-4 p-4 h-3/6 sm:grid-cols-2 sm:h-3/6">
-          <div className="rounded-lg shadow-md">
+        <div className="grid grid-cols-2 h-1/2 w-full space-x-4 text-2xl pb-24 ">
+          <div className="bg-white rounded-lg shadow-md">
             <img
               className="h-full w-full object-cover"
-              src="https://cdn.arstechnica.net/wp-content/uploads/2022/07/20220726_161534.jpg"
+              src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1640&q=80"
               alt="description of your image"
             />
-            <figcaption className="text-center mt-4 mb-4">
-              Electronics
-            </figcaption>
+            <figcaption className="text-center p-8">Electronics</figcaption>
           </div>
-          <div className="rounded-lg shadow-md">
+
+          <div className="bg-white rounded-lg shadow-md">
             <img
               className="h-full w-full object-cover"
-              src="https://images.unsplash.com/photo-1545147986-a9d6f2ab03b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fHBsYW50fGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+              src="https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1744&q=80"
               alt="description of your image"
             />
-            <figcaption className="text-center mt-4 mb-4">Others</figcaption>
+            <figcaption className="text-center p-8">Others</figcaption>
           </div>
         </div>
       </div>
